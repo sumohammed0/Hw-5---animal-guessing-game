a2c95ddd6ad5fc16f38bf7fccfac13aec063db8d
Safa Mohammed <sumohammed0@gmail.com>
2022-04-09 15:59:32 -0500

    Initial Commit

---
+++ Hw 5 - animal guessing game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hw 5 - animal guessing game
+//
+//  Created by Safa Mohammed on 4/9/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
